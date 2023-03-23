b2d1e4263d4a4234074f6ce50060ed7c9b8f28b2
Adam Grzybowski <adam.grzybowski@swmansion.com>
2023-03-23 13:56:23 +0100

    create responsive stack navigator

--- src/libs/Navigation/AppNavigator/AuthScreens.js
+++ src/libs/Navigation/AppNavigator/AuthScreens.js
@@ -20,7 +20,7 @@ import Navigation from '../Navigation';
 import * as User from '../../actions/User';
 import * as Modal from '../../actions/Modal';
 import modalCardStyleInterpolator from './modalCardStyleInterpolator';
-import createCustomModalStackNavigator from './createCustomModalStackNavigator';
+import createResponsiveStackNavigator from './ResponsiveStackNavigator/createResponsiveStackNavigator';
 import NotFoundPage from '../../../pages/ErrorPage/NotFoundPage';
 import getCurrentUrl from '../currentUrl';
 
@@ -65,7 +65,7 @@ Onyx.connect({
     },
 });
 
-const RootStack = createCustomModalStackNavigator();
+const RootStack = createResponsiveStackNavigator();
 
 // We want to delay the re-rendering for components(e.g. ReportActionCompose)
 // that depends on modal visibility until Modal is completely closed and its focused
@@ -163,6 +163,7 @@ class AuthScreens extends React.Component {
 
         return (
             <RootStack.Navigator
+                isNarrowLayout={this.props.isSmallScreenWidth}
                 mode="modal"
 
                 // We are disabling the default keyboard handling here since the automatic behavior is to close a
